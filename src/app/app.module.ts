29916c3eff608407b945e6d6fd34748e78d4321f
KevinTGC2002 <crafterthegolden@gmail.com>
2020-11-20 11:05:04 -0500

    5 files modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { LightboxModule } from 'ngx-lightbox';
+import { LightboxModule } from "ngx-lightbox";
 
 import {
   AccordionModule,
-  AlertModule,        // Foundation Callouts
+  AlertModule, // Foundation Callouts
   ButtonsModule,
-  CarouselModule,     // Foundation Orbit
+  CarouselModule, // Foundation Orbit
   CollapseModule,
   BsDatepickerModule,
-  BsDropdownModule,   // Foundation Dropdown Menus and Dropdown Panes
-  ModalModule,        // Foundation Reveal
+  BsDropdownModule, // Foundation Dropdown Menus and Dropdown Panes
+  ModalModule, // Foundation Reveal
   OffcanvasModule,
   PaginationModule,
   ProgressbarModule,
@@ -23,22 +23,25 @@ import {
   TabsModule,
   TimepickerModule,
   TooltipModule,
-  TypeaheadModule,
-} from 'ngx-foundation';
+  TypeaheadModule
+} from "ngx-foundation";
 
-
-
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartService } from './cart.service';
-import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
-import { FngAccordionComponent } from './fng-accordion/fng-accordion.component';
-import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
-import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-lightbox.component';
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
+import { ShippingComponent } from "./shipping/shipping.component";
+import { FngAccordionComponent } from "./fng-accordion/fng-accordion.component";
+import { GraphicDesignComponent } from "./graphic-design/graphic-design.component";
+import { PortfolioLightboxComponent } from "./portfolio-lightbox/portfolio-lightbox.component";
+import { MyResumeComponent } from "./my-resume/my-resume.component";
+import { ResumeEducationComponent } from "./my-resume/resume-education/resume-education.component";
+import { ResumeFooterComponent } from "./my-resume/resume-footer/resume-footer.component";
+import { ResumeProfileComponent } from "./my-resume/resume-profile/resume-profile.component";
+import { ResumeWorkExperienceComponent } from "./my-resume/resume-work-experience/resume-work-experience.component";
 
 @NgModule({
   imports: [
@@ -51,11 +54,12 @@ import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-light
     OffcanvasModule.forRoot(),
 
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
-      { path: 'graphic', component: GraphicDesignComponent },
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent },
+      { path: "graphic", component: GraphicDesignComponent },
+      { path: "resume", component: MyResumeComponent }
     ])
   ],
   declarations: [
@@ -68,13 +72,17 @@ import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-light
     ShippingComponent,
     FngAccordionComponent,
     GraphicDesignComponent,
-    PortfolioLightboxComponent
+    PortfolioLightboxComponent,
+    MyResumeComponent,
+    ResumeEducationComponent,
+    ResumeFooterComponent,
+    ResumeProfileComponent,
+    ResumeWorkExperienceComponent
   ],
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   providers: [CartService]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,88 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: "Phone XL",
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: "A large phone with one of the best screens"
   },
   {
-    name: 'Phone Mini',
+    name: "Phone Mini",
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: "A great phone with one of the best cameras"
   },
   {
-    name: 'Phone Standard',
+    name: "Phone Standard",
     price: 299,
-    description: ''
+    description: ""
   }
 ];
 
+export const r1 = {
+  profile: {
+    firstName: "Kevin",
+    lastName: "Aucancela",
+    isAlive: true,
+    age: 18,
+    photo: "https://www.w3schools.com/howto/img_avatar.png",
+    profession: "Designer",
+    address: {
+      streetAddress: "21 2nd Street",
+      city: "New York ",
+      state: "NY",
+      postalCode: "10021-3100",
+      country: "USA",
+      email: "ex@mail.com"
+    },
+    phoneNumbers: [
+      {
+        type: "home",
+        number: "212 555-1234"
+      },
+      {
+        type: "office",
+        number: "646 555-4567"
+      }
+    ],
+    languages: [
+      {
+        language: "English",
+        level: "60%"
+      },
+
+      {
+        language: "Spanish",
+        level: "90%"
+      },
+      {
+        language: "German",
+        level: "25%"
+      }
+    ],
+
+    skills: [
+      {
+        skill: "Adobe Photoshop",
+        level: "100%"
+      },
+
+      {
+        skill: "Photography",
+        level: "80%"
+      },
+      {
+        skill: "Illustrator",
+        level: "85%"
+      },
+
+      {
+        skill: "Media",
+        level: "50%"
+      }
+    ]
+  },
+
+  workExperience: [],
+  education: []
+};
 
 /*
 Copyright Google LLC. All Rights Reserved.
